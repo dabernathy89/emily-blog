4e4fec0d09001ef9a9477fed57e39c49b8fc5ad3
Daniel Abernathy <daniel@danielabernathy.com>
2026-02-16 15:39:33 -0600

    deployment fixes

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 # Build locally for the VM's architecture. --builder default prevents
 # accidentally building on the VM if the active Docker builder is emilyblog.
 docker buildx build --builder default --platform linux/amd64 --target production --load -t statamic-cms:latest .
-docker save statamic-cms:latest | gzip | ssh dabernathy@192.168.100.151 'gunzip | docker load'
+docker save statamic-cms:latest | gzip | pv | ssh dabernathy@192.168.100.151 'gunzip | docker load'
 docker --context emilyblog stack deploy -c docker-stack-cms.yml statamic
 
 # Swarm won't restart a service when the image tag (latest) hasn't changed,
@@ -14,5 +14,5 @@ docker --context emilyblog service update --force statamic_statamic
 echo "Waiting for new container to be ready..."
 sleep 15
 
-echo "Clearing Stache cache..."
-docker --context emilyblog exec $(docker --context emilyblog ps -q --filter name=statamic_statamic) php please stache:clear
+echo "Refreshing Stache cache..."
+docker --context emilyblog exec $(docker --context emilyblog ps -q --filter name=statamic_statamic) php please stache:refresh
